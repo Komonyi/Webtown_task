f103a89d70342ee64cd715092fb657ed8e41cd57
unknown <komonyilaszlo@gmail.com>
2019-08-17 14:40:07 +0200

    Missed a change to the DB loader from previous commit

--- Database/Loader.php
+++ Database/Loader.php
@@ -1,9 +1,7 @@
 <?php
 
-include_once("Entity/Product.php");
-
-function loadProducts() {
-    $products = json_decode(file_get_contents("Database/Products.json"), true);
+function loadProducts($path) {
+    $products = json_decode(file_get_contents($path), true);
 
     foreach ($products as &$product) {
         $product = new Product($product["id"], $product["name"], $product["price"], $product["megapack"]);
